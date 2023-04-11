7907f3ae196fb8dd7a6adb88e375b5016c259e24
Dmitry <dmitry@goverland.xyz>
2023-04-11 17:36:51 +0300

    Add layer for consuming messages from the nats jet stream

---
+++ events/proposal.go
@@ -0,0 +1,36 @@
+package events
+
+import "encoding/json"
+
+type (
+	SpacePayload struct {
+		ID   string `json:"id"`
+		Name string `json:"name"`
+	}
+
+	ProposalPayload struct {
+		ID       string         `json:"id"`
+		Title    string         `json:"title"`
+		Body     string         `json:"body"`
+		Choices  []string       `json:"choices"`
+		Start    int64          `json:"start"`
+		End      int64          `json:"end"`
+		Snapshot string         `json:"snapshot"`
+		State    string         `json:"state"`
+		Author   string         `json:"author"`
+		Spaces   []SpacePayload `json:"spaces"`
+	}
+
+	ProposalCreateHandler func(ProposalPayload) error
+)
+
+func (h ProposalCreateHandler) RawHandler() RawMessageHandler {
+	return func(raw []byte) error {
+		var d ProposalPayload
+		if err := json.Unmarshal(raw, &d); err != nil {
+			return err
+		}
+
+		return h(d)
+	}
+}
---
+++ examples/base_usage.go
@@ -0,0 +1,70 @@
+package examples
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"sync"
+	"time"
+
+	"github.com/nats-io/nats.go"
+
+	"github.com/goverland-labs/platform-events/events"
+	"github.com/goverland-labs/platform-events/pkg/natsclient"
+)
+
+func main() {
+	conn, err := nats.Connect(nats.DefaultURL)
+	if err != nil {
+		log.Fatal("connect:", err)
+	}
+	defer conn.Close()
+
+	subject := "test.events"
+	pr, err := natsclient.NewProducer(conn, subject)
+	if err != nil {
+		log.Fatal("new producer:", err)
+	}
+
+	go func() {
+		for i := 0; i < 5; i++ {
+			pl := events.ProposalPayload{ID: fmt.Sprintf("id-%d", i), Title: fmt.Sprintf("title #%d", i*10)}
+			if err = pr.PublishJSON(context.Background(), pl); err != nil {
+				log.Fatal("publish:", err)
+			}
+		}
+	}()
+
+	handler := func() events.ProposalCreateHandler {
+		return func(payload events.ProposalPayload) error {
+			fmt.Printf("message from nats: %s / %s \n", payload.ID, payload.Title)
+
+			return nil
+		}
+	}
+
+	group := "example"
+	con, err := natsclient.NewConsumer(context.Background(), conn, group, subject, handler())
+	if err != nil {
+		log.Fatal("new consumer:", err)
+	}
+
+	defer func() {
+		err = con.Close()
+		if err != nil {
+			log.Fatal("consumer close:", err)
+		}
+	}()
+
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	go func(wg *sync.WaitGroup) {
+		<-time.After(time.Second * 2)
+
+		fmt.Println("wait is done")
+		wg.Done()
+	}(&wg)
+	wg.Wait()
+
+	log.Printf("all done")
+}
