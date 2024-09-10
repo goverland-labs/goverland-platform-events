8d471fdaa2eacb02db493a249a69a2de8f8d368d
gov-dmitry <129734650+gov-dmitry@users.noreply.github.com>
2024-09-10 15:43:19 +0300

    Describe the internal skeleton of the delegate event (#34)

---
+++ events/core/delegates.go
@@ -0,0 +1,24 @@
+package core
+
+import (
+	"github.com/google/uuid"
+
+	"github.com/goverland-labs/goverland-platform-events/events"
+)
+
+const (
+	SubjectDelegateCreateProposal = "core.delegates.create_proposal"
+)
+
+type DelegatePayload struct {
+	// Initiator is delegate address who initiate the event
+	Initiator string `json:"initiator"`
+	// Delegator is delegator address (delegator delegate voting power to the initiator)
+	Delegator string `json:"delegator"`
+	// DaoID is internal dao identifier
+	DaoID uuid.UUID `json:"dao_id"`
+	// ProposalID is internal proposal identifier
+	ProposalID string `json:"proposal_id"`
+}
+
+type DelegatesHandler = events.Handler[DelegatePayload]
