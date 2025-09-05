9c9a87b72cd2fb4d9fe6aae7907ba4ada63b78a5
Dmitry <dmitry@goverland.xyz>
2025-09-05 15:38:15 +0300

    ERC20 Delegates subject

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,11 @@ to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
 ## [Unreleased]
 
+## [0.3.16] - 2025-09-05
+
+### Added
+- ERC20 Delegates subject
+
 ## [0.3.15] - 2025-06-16
 
 ### Added
---
+++ events/aggregator/delegates_erc20.go
@@ -0,0 +1,21 @@
+package aggregator
+
+import (
+	"github.com/goverland-labs/goverland-platform-events/events"
+)
+
+const (
+	SubjectDelegateERC20 = "aggregator.erc20indexer.update"
+)
+
+type ERC20DelegatePayload struct {
+	AddressFrom    string `json:"address_from"`
+	AddressTo      string `json:"address_to"`
+	Token          string `json:"token"`
+	Network        string `json:"network"`
+	BlockNumber    int    `json:"block_number"`
+	BlockTimestamp int    `json:"block_timestamp"`
+	VotingPower    int    `json:"voting_power"`
+}
+
+type ERC20DelegateHandler = events.Handler[ERC20DelegatePayload]
